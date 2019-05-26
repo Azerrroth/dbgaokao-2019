1e496add069a24a42e09e6818f265edbc4fff83c
only123hhl <1198651380@qq.com>
2019-05-26 11:25:30 +0800

    布局还没完善

---
+++ ChangeLayout/ConnectDB.py
@@ -0,0 +1,126 @@
+from PyQt5 import QtCore, QtGui, QtWidgets, QtSql
+def connectDB(HostName,DbName, UserName, PassWord ):
+    db = QtSql.QSqlDatabase.addDatabase('QMYSQL')
+    db.setHostName(HostName)
+    db.setDatabaseName(DbName)
+    db.setUserName(UserName)
+    db.setPassword(PassWord)
+    db.setPort(3306) # 端口号
+    db.open()
+    return db
+
+def dataset1(table, db, str1):
+    query=QtSql.QSqlQuery(db)
+    query.prepare("select course_id,sec_id,semester,year,building,room_number from %s where  time_slot_id='%s' "%(table, str1))
+    a=[]
+    length=rows=0
+    if(query.exec()):
+        #print("good")
+        length=len(query.record())
+        rows=query.size()
+        while(query.next()): 
+            b=[]
+            for i in range(len(query.record())):
+                b.append(query.value(i))
+            a.append(b)
+        #print(length)
+    #print(a)
+    return a, length, rows
+
+
+
+def dataset3(table, db, str1, str2, str3):
+    query=QtSql.QSqlQuery(db)
+    query.prepare("select sec_id,year,room_number,time_slot_id from %s where course_id='%s' and semester='%s' and building='%s' "%(table, str1, str2, str3))
+    a=[]
+    length=rows=0
+    if(query.exec()):
+        #print("good")
+        length=len(query.record())
+        rows=query.size()
+        while(query.next()): 
+            b=[]
+            for i in range(len(query.record())):
+                b.append(query.value(i))
+            a.append(b)
+        #print(length)
+    #print(a)
+    return a, length, rows
+
+
+
+def selectdistinct(col, table, db):
+    query=QtSql.QSqlQuery(db)
+    query.prepare("select distinct %s from %s"%(col, table))
+    a=[]
+    if(query.exec()):
+       while(query.next()): 
+         a.append(query.value(0))
+    return a
+class Ui_MainWindow(object):
+    db=connectDB('127.0.0.1', 'hhlschema', 'HHL', '12345')
+    
+    #学院列表
+    l=selectdistinct('dept_name', 'Student', db)
+    s=""
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(800, 600)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
+        self.centralwidget.setObjectName("centralwidget")
+        self.pushButton = QtWidgets.QPushButton(self.centralwidget)
+        self.pushButton.setGeometry(QtCore.QRect(110, 120, 93, 28))
+        self.pushButton.setObjectName("pushButton")
+        self.tableView = QtWidgets.QTableView(self.centralwidget)
+        self.tableView.setGeometry(QtCore.QRect(340, 240, 256, 192))
+        self.tableView.setObjectName("tableView")
+        
+        self.tableView.setHidden(True)
+        self.comboBox = QtWidgets.QComboBox(self.centralwidget)
+        self.comboBox.setGeometry(QtCore.QRect(390, 120, 87, 22))
+        self.comboBox.setObjectName("comboBox")
+        
+        
+        self.comboBox.addItems(self.l)
+        
+        #self.s=self.comboBox.currentText()
+        self.comboBox.activated[str].connect(self.changetext)
+        
+        
+        
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+        self.pushButton.clicked.connect(self.tableView.show)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
+
+    def retranslateUi(self, MainWindow):
+        _translate = QtCore.QCoreApplication.translate
+        MainWindow.setWindowTitle(_translate("MainWindow", "MainWindow"))
+        self.pushButton.setText(_translate("MainWindow", "提交"))
+        
+    #动态获取comboBox选中的内容
+    def changetext(self, text):
+        self.s=text
+        #print(self.s)
+        testModel =QtSql.QSqlTableModel()
+        self.tableView.setHidden(True)
+        testModel.setTable('Student')
+        testModel.setEditStrategy(QtSql.QSqlTableModel.OnManualSubmit)
+        testModel.setFilter("dept_name='%s'"%(self.s))
+        testModel.select()
+        self.tableView.setModel(testModel)
+         
+    def closerevent(self, event):
+        self.db.close()
+    
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    
+    MainWindow = QtWidgets.QMainWindow()
+    ui = Ui_MainWindow()
+    #print(ui.s)
+    ui.setupUi(MainWindow)
+    MainWindow.show()
+    sys.exit(app.exec_())
---
+++ ChangeLayout/Ui_ChildrenForm3.py
@@ -0,0 +1,68 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'C:\Users\黄翰林\Desktop\数据库大作业\前端制作\GaoKao\ChildrenForm3.ui'
+#
+# Created by: PyQt5 UI code generator 5.11.3
+#
+# WARNING! All changes made in this file will be lost!
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+from PyQt5.QtGui import*
+from PyQt5.QtCore import *
+from PyQt5.QtGui import*
+from PyQt5.QtCore import *
+
+class Ui_ChildrenFrom3(object):
+    def setupUi(self, ChildrenFrom3):
+        ChildrenFrom3.setObjectName("ChildrenFrom3")
+        ChildrenFrom3.resize(1200, 1000)
+        self.tableWidget = QtWidgets.QTableWidget(ChildrenFrom3)
+        self.tableWidget.setGeometry(QtCore.QRect(200, 60, 530, 180))
+        self.tableWidget.setObjectName("tableWidget")
+        self.tableWidget.setColumnCount(3)
+        self.tableWidget.setRowCount(3)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setVerticalHeaderItem(2, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(0, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(1, item)
+        item = QtWidgets.QTableWidgetItem()
+        self.tableWidget.setHorizontalHeaderItem(2, item)
+
+        self.retranslateUi(ChildrenFrom3)
+        QtCore.QMetaObject.connectSlotsByName(ChildrenFrom3)
+
+    def retranslateUi(self, ChildrenFrom3):
+        _translate = QtCore.QCoreApplication.translate
+        ChildrenFrom3.setWindowTitle(_translate("ChildrenFrom3", "Form"))
+        item = self.tableWidget.verticalHeaderItem(0)
+        item.setText(_translate("ChildrenFrom3", "Name"))
+        item = self.tableWidget.verticalHeaderItem(1)
+        item.setText(_translate("ChildrenFrom3", "Height"))
+        item = self.tableWidget.verticalHeaderItem(2)
+        item.setText(_translate("ChildrenFrom3", "Weight"))
+        item = self.tableWidget.horizontalHeaderItem(0)
+        item.setText(_translate("ChildrenFrom3", "HHL"))
+        item = self.tableWidget.horizontalHeaderItem(1)
+        item.setText(_translate("ChildrenFrom3", "HZL"))
+        item = self.tableWidget.horizontalHeaderItem(2)
+        item.setText(_translate("ChildrenFrom3", "LHZ"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    palette=QPalette()
+    palette.setColor(QPalette.Background, QColor(202, 216, 235))
+    ChildrenFrom3 = QtWidgets.QWidget()
+    ChildrenFrom3.setPalette(palette)
+    ui = Ui_ChildrenFrom3()
+    ui.setupUi(ChildrenFrom3)
+    ChildrenFrom3.show()
+    sys.exit(app.exec_())
+
